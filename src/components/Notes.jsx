b1ad3d9a189fe8dd2b5cc14139340aa657b59029
Upeksha Wanniarachchi <49319444+uwanni@users.noreply.github.com>
2023-04-27 23:02:28 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DevTown</h1>
+      <p>
+        We learnt the basics of Web-Development, JavaScript, JSX, React,
+        ReactDOM, and created a project at the very end of the bootcamp as well.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
